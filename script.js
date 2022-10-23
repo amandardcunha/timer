1c9cef757b44b7c1d89b74e440f2a8c46d519273
amandardcunha <amandardcunha@hotmail.com>
2022-10-23 09:07:57 -0400

    Add code to get circle, set timer duration on timer start and animate circle on tick

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const durationInput = document.querySelector('#duration');
 const startButton =  document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector("circle");
+
+const circumference = 2 * Math.PI * circle.getAttribute('r');
+let duration = 0;
+circle.setAttribute("stroke-dasharrary", circumference);
 
 const timer = new Timer(durationInput,startButton, pauseButton,{
-    onStart() {
-        console.log("Timer Started!")
+    onStart(totalDuration) {
+        duration = totalDuration;
     },
-    onTick(){
-        console.log("Timer Ticked...")
+    onTick(timeRemaining){
+        circle.setAttribute("stroke-dashoffset", 
+                circumference * timeRemaining / duration - circumference
+                );
     },
     onComplete(){
         console.log("Time completed!")
